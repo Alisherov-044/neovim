213942e88a48d85faa83654408dbdb6b647931d3
Sadulla Alisherov <alisherov044@gmail.com>
2024-05-29 00:28:00 +0500

    feat: neovim setup

---
+++ lua/setup/remap.lua
@@ -0,0 +1,6 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>q", vim.cmd.q, {})
+vim.keymap.set("n", "<leader>so", vim.cmd.so, {})
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, {})
+vim.keymap.set("n", "<leader>s", vim.cmd.w, {})
+vim.keymap.set("i", "jj", "<Esc>", {})
